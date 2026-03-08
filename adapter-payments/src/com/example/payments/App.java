cb8167afe904cafed2c42ac182736ef64b70a98c
Namami Verma <Namami.24bcs10349@sst.scaler.com>
2026-03-08 22:43:22 +0530

    Adapter-Payments Refactored

--- adapter-payments/src/com/example/payments/App.java
+++ adapter-payments/src/com/example/payments/App.java
@@ -9,10 +9,14 @@ public class App {
         // TODO: register adapters instead of raw SDKs
         // gateways.put("fastpay", new FastPayAdapter(new FastPayClient()));
         // gateways.put("safecash", new SafeCashAdapter(new SafeCashClient()));
+        gateways.put("fastpay", new FastPayAdapter(new FastPayClient()));
+        gateways.put("safecash", new SafeCashAdapter(new SafeCashClient()));
+
         OrderService svc = new OrderService(gateways);
 
         String id1 = svc.charge("fastpay", "cust-1", 1299);
         String id2 = svc.charge("safecash", "cust-2", 1299);
+
         System.out.println(id1);
         System.out.println(id2);
     }
